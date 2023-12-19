90f4c8b87089e51ad2731674a92772e8c564fae4
SufiyarovDanil <danil-sufiyarov@mail.ru>
2023-12-19 20:26:15 +0500

    исправил url эндпоинтов

--- src/music/router.py
+++ src/music/router.py
@@ -22,7 +22,7 @@ async def get_all_bands() -> JSONResponse:
     return JSONResponse(content=jsonable_encoder(band_list), status_code=200)
 
 
-@router.get('/{id}')
+@router.get('band/{id}')
 async def get_band_by_id(id: int) -> JSONResponse:
     band = await service.get_band_by_id(id)
 
@@ -32,21 +32,21 @@ async def get_band_by_id(id: int) -> JSONResponse:
     return JSONResponse(content=jsonable_encoder(band), status_code=200)
 
 
-@router.post('/')
+@router.post('/band')
 async def add_band(band: BandSchema) -> Response:
     await service.add_band(band.name, band.created_at)
 
     return Response(status_code=200)
 
 
-@router.put('/')
+@router.put('/band')
 async def update_band(band: BandSchema) -> Response:
     await service.update_band(band.id, band.name, band.created_at)
 
     return Response(status_code=200)
 
 
-@router.delete('/', tags=['band'], response_class=Response)
+@router.delete('/band')
 async def delete_band(id: int) -> Response:
     await service.delete_band(id)
 
