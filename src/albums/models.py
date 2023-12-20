dc7e15398db6f9e3a0976e2ab2720bb2aa2514a3
SufiyarovDanil <danil-sufiyarov@mail.ru>
2023-12-20 12:44:21 +0500

    декомпозировал модуль models

---
+++ src/albums/models.py
@@ -0,0 +1,64 @@
+from sqlalchemy import SmallInteger, BigInteger, VARCHAR, Date, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database import BaseModel
+from bands.models import BandModel, GenreModel
+
+
+class AlbumModel(BaseModel):
+    __tablename__ = 'album'
+
+    id: Mapped[BigInteger] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=True,
+        name='pk_id'
+    )
+    name: Mapped[VARCHAR(150)] = mapped_column(
+        VARCHAR(150),
+        nullable=False,
+        name='name'
+    )
+    published_at: Mapped[Date] = mapped_column(
+        Date,
+        nullable=True,
+        name='published_at'
+    )
+    band_id: Mapped[BigInteger] = mapped_column(
+        BigInteger,
+        ForeignKey('band.pk_id', ondelete='SET NULL'),
+        nullable=False,
+        name='fk_band_id'
+    )
+
+    band: Mapped['BandModel'] = relationship(back_populates='albums')
+    tracks: Mapped[list['TrackModel']] = relationship(back_populates='album')
+    genres: Mapped[list['GenreModel']] = relationship(secondary='album_genre')
+
+
+class TrackModel(BaseModel):
+    __tablename__ = 'track'
+
+    id: Mapped[BigInteger] = mapped_column(
+        BigInteger,
+        primary_key=True,
+        autoincrement=True,
+        name='pk_id'
+    )
+    name: Mapped[VARCHAR] = mapped_column(
+        VARCHAR(128),
+        nullable=False,
+        name='name'
+    )
+    length: Mapped[SmallInteger] = mapped_column(
+        SmallInteger,
+        nullable=False,
+        name='length'
+    )
+    album_id: Mapped[BigInteger] = mapped_column(
+        BigInteger,
+        ForeignKey('album.pk_id', ondelete='CASCADE'),
+        nullable=False,
+        name='fk_album_id'
+    )
+
+    album: Mapped['AlbumModel'] = relationship(back_populates='tracks')
--- src/bands/models.py
+++ src/bands/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import SmallInteger, BigInteger, VARCHAR, Date, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import BaseModel
+from albums.models import AlbumModel
 
 
 class BandModel(BaseModel):
@@ -24,37 +25,6 @@ class BandModel(BaseModel):
     )
 
     albums: Mapped[list['AlbumModel']] = relationship(back_populates='band')
-
-
-class AlbumModel(BaseModel):
-    __tablename__ = 'album'
-
-    id: Mapped[BigInteger] = mapped_column(
-        BigInteger,
-        primary_key=True,
-        autoincrement=True,
-        name='pk_id'
-    )
-    name: Mapped[VARCHAR(150)] = mapped_column(
-        VARCHAR(150),
-        nullable=False,
-        name='name'
-    )
-    published_at: Mapped[Date] = mapped_column(
-        Date,
-        nullable=True,
-        name='published_at'
-    )
-    band_id: Mapped[BigInteger] = mapped_column(
-        BigInteger,
-        ForeignKey('band.pk_id', ondelete='SET NULL'),
-        nullable=False,
-        name='fk_band_id'
-    )
-
-    band: Mapped['BandModel'] = relationship(back_populates='albums')
-    tracks: Mapped[list['TrackModel']] = relationship(back_populates='album')
-    genres: Mapped[list['GenreModel']] = relationship(secondary='album_genre')
 
 
 class GenreModel(BaseModel):
@@ -91,32 +61,3 @@ class AlbumGenreModel(BaseModel):
         nullable=False,
         name='fk_genre_id'
     )
-
-
-class TrackModel(BaseModel):
-    __tablename__ = 'track'
-
-    id: Mapped[BigInteger] = mapped_column(
-        BigInteger,
-        primary_key=True,
-        autoincrement=True,
-        name='pk_id'
-    )
-    name: Mapped[VARCHAR] = mapped_column(
-        VARCHAR(128),
-        nullable=False,
-        name='name'
-    )
-    length: Mapped[SmallInteger] = mapped_column(
-        SmallInteger,
-        nullable=False,
-        name='length'
-    )
-    album_id: Mapped[BigInteger] = mapped_column(
-        BigInteger,
-        ForeignKey('album.pk_id', ondelete='CASCADE'),
-        nullable=False,
-        name='fk_album_id'
-    )
-
-    album: Mapped['AlbumModel'] = relationship(back_populates='tracks')
