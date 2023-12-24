632b4d06f2f7c473c3d29881cd077c39f178a7e7
SufiyarovDanil <danil-sufiyarov@mail.ru>
2023-12-24 21:26:28 +0500

    добавил новые исключения

--- src/albums/exceptions.py
+++ src/albums/exceptions.py
@@ -5,3 +5,15 @@ class AlbumNotFound(HTTPException):
     def __init__(self):
         self.status_code = 404
         self.detail = 'album not found'
+
+
+class FailedToCreateAlbum(HTTPException):
+    def __init__(self):
+        self.status_code = 507
+        self.detail = 'failed to insert new album data'
+
+
+class FailedToUpdateAlbum(HTTPException):
+    def __init__(self):
+        self.status_code = 500
+        self.detail = 'failed to update album data'
